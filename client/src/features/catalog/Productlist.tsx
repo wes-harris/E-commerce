92e93b9a85a2a681ae9e51bf0a16b502d9ee82e0
Wesley Harris <harriscaws@gmail.com>
2024-10-14 17:20:03 -0400

    Added style to the product cards

--- client/src/features/catalog/Productlist.tsx
+++ client/src/features/catalog/Productlist.tsx
@@ -10,7 +10,7 @@ export default function ProductList({ products }: Props) {
   return (
     <Grid2 container spacing={4}>
       {products.map((product) => (
-        <Grid2 spacing={4} key={product.id}>
+        <Grid2 size={3} key={product.id}>
           <ProductCard product={product} />
         </Grid2>
       ))}
