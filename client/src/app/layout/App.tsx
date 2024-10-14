479562abb17efdeeb141c73eceb6b6f3627d62a2
Wesley Harris <harriscaws@gmail.com>
2024-10-14 11:42:13 -0400

    Destructured properties out of products

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../models/product';
+import Catalog from '../../features/catalog/catalog';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -27,14 +28,7 @@ function App() {
   return (
     <div>
       <h1>Re-Store</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - {product.price}
-          </li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add product</button>
+      <Catalog products={products} addProduct={addProduct} />
     </div>
   );
 }
