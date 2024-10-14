96dee4402ab0b2567c372a029576ca050702e06b
Wesley Harris <harriscaws@gmail.com>
2024-10-14 13:36:50 -0400

    Added the Material UI styling framework

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../models/product';
 import Catalog from '../../features/catalog/catalog';
+import { Typography } from '@mui/material';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -27,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h1>Re-Store</h1>
+      <Typography variant="h1">Re-Store</Typography>
       <Catalog products={products} addProduct={addProduct} />
     </div>
   );
