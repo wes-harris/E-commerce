8a6f9d69575bb8ba0c2604a738bf59f893451cdf
Wesley Harris <harriscaws@gmail.com>
2024-10-14 21:31:31 -0400

    replaced fetch with Axios

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,25 @@
 import { Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Product } from '../../app/models/product';
+import axios from 'axios';
 
 export default function ProductDetails() {
-  return <Typography variant="h2">Product details</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProducts] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/Products/${id}`)
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (!product) return <h3>Product not found</h3>;
+
+  return <Typography variant="h2">{product.name}</Typography>;
 }
