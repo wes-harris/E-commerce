a5cd199681a1f50ad20b7befe80ea1b642dcb667
Wesley Harris <harriscaws@gmail.com>
2024-10-17 17:24:17 -0400

    created a server error component

--- client/src/app/router/route.tsx
+++ client/src/app/router/route.tsx
@@ -5,6 +5,7 @@ import ContactPage from '../../features/contact/ContactPage';
 import Catalog from '../../features/catalog/Catalog';
 import HomePage from '../../features/home/HomePage';
 import App from '../layout/App';
+import ServerError from '../errors/ServerError';
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ export const router = createBrowserRouter([
       { path: 'catalog/:id', element: <ProductDetails /> },
       { path: 'about', element: <AboutPage /> },
       { path: 'contact', element: <ContactPage /> },
+      { path: 'server-error', element: <ServerError /> },
     ],
   },
 ]);
