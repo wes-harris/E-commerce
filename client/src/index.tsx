49a922c56cb3e3c6816cdd1ff22465e939fbbea0
Wesley Harris <harriscaws@gmail.com>
2024-10-18 17:40:31 -0400

    used React context to centralise state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,9 +7,12 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/route.js';
+import { StoreProvider } from './app/Context/StoreContext.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </React.StrictMode>
 );
