a5d819b2b95ac81df356cf92f68c7a68d7aa0b44
Wesley Harris <harriscaws@gmail.com>
2024-10-20 20:40:01 -0400

    action creators

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,13 +1,39 @@
-import { Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { CounterState } from './counterReducer';
+import { Button, ButtonGroup, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { CounterState, decrement, increment } from './counterReducer';
 
 export default function ContactPage() {
-  const { data, title } = useSelector((state: CounterState) => state);
+  const dispatch = useDispatch();
+  const data = useSelector((state: CounterState) => state.data);
+  const title = useSelector((state: CounterState) => state.title);
+
   return (
     <>
       <Typography variant="h2">{title}</Typography>
       <Typography variant="h5">The data is: {data}</Typography>
+      <ButtonGroup>
+        <Button
+          onClick={() => dispatch(decrement())}
+          variant="contained"
+          color="error"
+        >
+          Decrement
+        </Button>
+        <Button
+          onClick={() => dispatch(increment())}
+          variant="contained"
+          color="primary"
+        >
+          Increment
+        </Button>
+        <Button
+          onClick={() => dispatch(increment(5))}
+          variant="contained"
+          color="secondary"
+        >
+          Increment by 5
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
--- client/src/features/contact/counterReducer.ts
+++ client/src/features/contact/counterReducer.ts
@@ -1,6 +1,10 @@
+export const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+export const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+
+
 export interface CounterState {
-    data: number
-    title: string
+    data: number;
+    title: string;
 }
 
 const initialState: CounterState = {
@@ -8,6 +12,38 @@ const initialState: CounterState = {
     title: 'YARC (yet another redux counter)'
 }
 
-export default function counterReducer(state = initialState, action: any) {
-    return state;
+export function increment(amount = 1) {
+    return {
+        type: INCREMENT_COUNTER,
+        payload: amount
+    }
+}
+
+export function decrement(amount = 1) {
+    return {
+        type: DECREMENT_COUNTER,
+        payload: amount
+    }
+}
+
+interface CounterAction {
+    type: string
+    payload: number
+}
+
+export default function counterReducer(state = initialState, action: CounterAction) {
+    switch (action.type) {
+        case INCREMENT_COUNTER:
+            return {
+                ...state,
+                data: state.data + action.payload
+            }
+        case DECREMENT_COUNTER:
+            return {
+                ...state,
+                data: state.data - action.payload
+            }
+        default:
+            return state;
+    }
 }
